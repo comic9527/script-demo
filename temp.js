7bbb222b0493760d906dea4d1fd15fbcd2526953
bysong <bysong@mail.ustc.edu.cn.>
2025-01-13 18:13:14 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         firstscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-13
+// @description  try to take over the world!
+// @author       bys
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+    'use strict';
+
+    console.log('hello world');
+    }
+)();
